9e3488e619020e3065af302bac7065969e1f8c15
shivam choudhary <shivam64128@gmail.com>
2026-02-24 20:54:40 +0530

    Add custom keymaps for easier navigation and editing
    Added custom keymaps for insert and visual modes.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap("i", "jk", "<esc>", { noremap = false })
+
+vim.keymap.set("v", "j", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "k", ":m '<-2<cr>gv=gv")
+
+vim.keymap.set("n", "<leader>r", [[:%s/\<<c-r><c-w>\>/<c-r><c-w>/gi<left><left><left>]])
